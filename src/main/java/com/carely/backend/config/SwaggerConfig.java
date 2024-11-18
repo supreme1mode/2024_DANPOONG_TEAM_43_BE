89c3f279b20eb4b3e1e4bb88155d58bd2cbb3b16
7beunseo <kimes0403@gmail.com>
2024-11-18 12:57:40 +0900

    📝 [DOCS] Swagger 세팅 #4

---
+++ src/main/java/com/carely/backend/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.carely.backend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.examples.Example;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+
+        Server testServer = new Server();
+        testServer.setUrl("http://localhost:8080/");
+
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .description("토큰값을 입력하여 인증을 활성화할 수 있습니다.")
+                .bearerFormat("JWT")
+        );
+
+        OpenAPI openAPI = new OpenAPI()
+                .components(components)
+                .info(new Info()
+                        .title("2024 단풍톤 carely API")
+                        .description("2024 단풍톤 백엔드 carely API 명세서")
+                        .version("1.0.0"))
+                .addSecurityItem(securityRequirement)
+                .servers(List.of(testServer));
+
+
+        return openAPI;
+    }
+
+}
