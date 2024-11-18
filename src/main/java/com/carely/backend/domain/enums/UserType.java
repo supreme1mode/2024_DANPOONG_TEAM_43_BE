041028297df2cf247987de55b271b143f7527043
7beunseo <kimes0403@gmail.com>
2024-11-18 12:10:12 +0900

    🗃️ [FEAT] User 엔터티 작성 #1

---
+++ src/main/java/com/carely/backend/domain/enums/UserType.java
@@ -0,0 +1,7 @@
+package com.carely.backend.domain.enums;
+
+public enum UserType {
+    CAREGIVER,
+    VOLUNTEER,
+    CARE_WORKER
+}
---
+++ src/main/java/com/carely/backend/domain/enums/VolunteerType.java
@@ -0,0 +1,6 @@
+package com.carely.backend.domain.enums;
+
+public enum VolunteerType {
+    VOLUNTEER_REQUEST, // 간병인이 요청
+    CARE_WORKER_REQUEST, // 자원봉사자가 요청
+}
