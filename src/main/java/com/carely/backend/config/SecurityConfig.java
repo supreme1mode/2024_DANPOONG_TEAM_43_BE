1604598785c21963f3410c6be208aaddcf716ec7
7beunseo <kimes0403@gmail.com>
2024-11-18 14:12:49 +0900

    🐛 [FIX] 403 에러 해결 #8

--- src/main/java/com/carely/backend/config/SecurityConfig.java
+++ src/main/java/com/carely/backend/config/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.carely.backend.config;
 
 
+import com.carely.backend.jwt.JWTFilter;
+import com.carely.backend.jwt.JWTUtil;
+import com.carely.backend.jwt.LoginFilter;
+import com.carely.backend.repository.RefreshRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +15,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -19,7 +29,8 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final AuthenticationConfiguration authenticationConfiguration;
-
+    private final JWTUtil jwtUtil;
+    private final RefreshRepository redisRepository;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
@@ -38,6 +49,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // CORS 설정 추가
+        http.cors().configurationSource(corsConfigurationSource()); // CORS 설정을 활성화
+
         // csrf 비활성화, 로그인 방식 비활성화
         http.csrf(AbstractHttpConfigurer::disable);
         http.formLogin(AbstractHttpConfigurer::disable);
@@ -46,14 +60,30 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers(
-                                "/login", "/error", "/register", "/reissue",
+                                "/login", "/error", "/register", "/kakao/login", "/reissue",
                                 "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**",
-                                "/chat/**", "/kakao-code"
+                                "/kakao-login", "/kakao-code", "/test", "/chat/**"
                         ).permitAll()
                         .anyRequest().authenticated());
 
+        // 필터 설정
+        http.addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, redisRepository), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Frontend의 Origin을 추가
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        configuration.setExposedHeaders(Arrays.asList("accessToken", "refreshToken"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
--- src/main/java/com/carely/backend/jwt/LoginFilter.java
+++ src/main/java/com/carely/backend/jwt/LoginFilter.java
@@ -1,7 +1,16 @@
 package com.carely.backend.jwt;
 
 
+import com.carely.backend.code.ErrorCode;
+import com.carely.backend.code.SuccessCode;
+import com.carely.backend.domain.RefreshEntity;
+import com.carely.backend.dto.response.ErrorResponseDTO;
+import com.carely.backend.dto.response.ResponseDTO;
+import com.carely.backend.dto.user.CustomUserDetails;
+import com.carely.backend.dto.user.UserResponseDTO;
 import com.carely.backend.repository.RefreshRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -9,8 +18,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 
 
 @RequiredArgsConstructor
@@ -32,5 +45,58 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
 
         return authenticationManager.authenticate(authToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException {
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        String username = customUserDetails.getUsername();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        // 요청 헤더에서 JWT 토큰 가져오기
+        String token = request.getHeader("Authorization").substring("Bearer ".length());
+
+        // JWT에서 userType 가져오기
+        String userType = jwtUtil.getUserType(token);
+
+        String accessToken = jwtUtil.createJwt("accessToken", username, role, userType, 86400000L);
+        String refreshToken = jwtUtil.createJwt("refreshToken", username, role, userType, 86400000L);
+
+        response.setHeader("accessToken", "Bearer " + accessToken);
+        response.setHeader("refreshToken", "Bearer " + refreshToken);
+
+        addRefreshEntity(refreshToken, username);
+
+        UserResponseDTO loginResponseDTO = new UserResponseDTO(userType);
+
+        ResponseDTO<UserResponseDTO> responseDTO = new ResponseDTO<>(SuccessCode.SUCCESS_LOGIN, loginResponseDTO);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(responseDTO);
+        response.getWriter().write(jsonResponse);
+    }
+
+
+    private void addRefreshEntity(String refresh, String username) {
+        RefreshEntity refreshEntity = new RefreshEntity(refresh, username);
+        redisRepository.save(refreshEntity);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException {
+
+        response.setStatus(401);
+
+        ErrorResponseDTO responseDTO = new ErrorResponseDTO(ErrorCode.USER_NOT_FOUND);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(responseDTO);
+        response.getWriter().write(jsonResponse);
     }
 }
