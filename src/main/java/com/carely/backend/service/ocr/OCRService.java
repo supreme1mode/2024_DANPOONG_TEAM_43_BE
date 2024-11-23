babf962f3aa5cacfffed8ee80bd813e090459d80
7beunseo <kimes0403@gmail.com>
2024-11-24 00:36:05 +0900

    🐛 [FIX] ocr 정규식 패턴 수정 #59

--- src/main/java/com/carely/backend/service/ocr/OCRService.java
+++ src/main/java/com/carely/backend/service/ocr/OCRService.java
@@ -75,6 +75,7 @@ public class OCRService {
 
         // 텍스트에서 필요한 정보 추출
         OCRResponseDto ocrResponseDto =  extractFields(extractedText);
+        System.out.println(ocrResponseDto.getName());
         if (ocrResponseDto.getName().equals(username)) {
             return ocrResponseDto;
         }
@@ -105,13 +106,13 @@ public class OCRService {
     // 정규식을 사용하여 필드 추출
     private OCRResponseDto extractFields(String ocrResult) {
         // 정규식 패턴
-        String namePattern = "성명\\s*(\\S+)";
+        String namePattern = "성\\s명\\s*(\\S+)";
         String dobPattern = "생년월일\\s*([\\d]{4}년\\s*[\\d]{2}월\\s*[\\d]{2}일)";
         String regNumberPattern = "등록 번호\\s*([A-Z0-9]+)";
         String categoryPattern = "자격종목\\s*(\\S+)";
         String acquisitionDatePattern = "취 득 일 자\\s*([\\d]{4}년\\s*[\\d]{2}월\\s*[\\d]{2}일)";
         String subjectPattern = "취득과목\\s*([\\S ]+)";
-
+        System.out.println(ocrResult);
         return OCRResponseDto.builder()
                 .name(extractByPattern(ocrResult, namePattern))
                 .birth(extractByPattern(ocrResult, dobPattern))
