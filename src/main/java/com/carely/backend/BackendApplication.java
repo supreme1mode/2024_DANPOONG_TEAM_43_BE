88d5caa524218b381441d78a731a997e2de2f7e5
이규민 <90364652+20210815@users.noreply.github.com>
2024-12-02 02:18:18 +0900

    🩹 [FIX] private key 누락되지 않게 적용

--- src/main/java/com/carely/backend/BackendApplication.java
+++ src/main/java/com/carely/backend/BackendApplication.java
@@ -1,11 +1,14 @@
 package com.carely.backend;
 
+import com.carely.backend.service.certificate.GanacheProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableConfigurationProperties(GanacheProperties.class)
 public class BackendApplication {
 
 	public static void main(String[] args) {
