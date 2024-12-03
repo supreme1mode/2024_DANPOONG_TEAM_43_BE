d2e7e7722e98d1e856d95957edf9872eeff2b8b6
이규민 <90364652+20210815@users.noreply.github.com>
2024-12-03 15:02:19 +0900

    Update SecurityConfig.java

--- src/main/java/com/carely/backend/config/SecurityConfig.java
+++ src/main/java/com/carely/backend/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/login", "/error", "/register", "/kakao/login", "/reissue",
                                 "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**"
-                                , "/kakao-code", "/test", "/chat/**", "/certificates/extract", "/test/", "/token", "/**"
+                                , "/kakao-code", "/test", "/chat/**", "/certificates/extract", "/test/",
                         ).permitAll()
                         .anyRequest().authenticated());
 
