ad990ec6b12642a4f86a272d982767d5c9d1944e
이규민 <90364652+20210815@users.noreply.github.com>
2024-12-05 23:29:47 +0900

    🩹 [FIX] 필드명 변경

--- src/main/java/com/carely/backend/dto/guestBook/ResponseGroupGuestbookDTO.java
+++ src/main/java/com/carely/backend/dto/guestBook/ResponseGroupGuestbookDTO.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class ResponseGroupGuestbookDTO {
-    private GuestBookDTO otherType;
+    private GuestBookDTO volunteer;
     private GuestBookDTO caregiver;
 
     @Getter
