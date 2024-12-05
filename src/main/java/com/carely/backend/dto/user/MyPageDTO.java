0097d49cf463e631f2b21826f92b617d29bb337d
이규민 <90364652+20210815@users.noreply.github.com>
2024-12-05 23:15:29 +0900

    ✨ [FEAT] 함꼐한 추억 추가 - 프로필

--- src/main/java/com/carely/backend/dto/user/MyPageDTO.java
+++ src/main/java/com/carely/backend/dto/user/MyPageDTO.java
@@ -2,6 +2,7 @@ package com.carely.backend.dto.user;
 
 import com.carely.backend.domain.User;
 import com.carely.backend.domain.enums.UserType;
+import com.carely.backend.dto.guestBook.ResponseGroupGuestbookDTO;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
@@ -9,6 +10,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+
 import org.json.JSONObject;
 import org.web3j.abi.datatypes.Int;
 
@@ -87,6 +90,9 @@ public class MyPageDTO {
         private String bath;
         private String walk;
         private String story;
+
+        @Setter
+        private List<ResponseGroupGuestbookDTO> guestbookDTOS = null;
         // 함께한 사람 setter 로 추가할 수 있도록 함
         @JsonInclude(JsonInclude.Include.NON_NULL)
         @Setter
