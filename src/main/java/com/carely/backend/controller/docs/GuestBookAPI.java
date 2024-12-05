44cb88f6daea8e23e4146f95c88e3d03ec19aa19
이규민 <90364652+20210815@users.noreply.github.com>
2024-12-05 18:50:57 +0900

    📝 [DOCUMENT

--- src/main/java/com/carely/backend/controller/docs/GuestBookAPI.java
+++ src/main/java/com/carely/backend/controller/docs/GuestBookAPI.java
@@ -189,7 +189,91 @@ public interface GuestBookAPI {
     })
     public ResponseEntity<ResponseDTO<?>> getGroupGuestbook(@PathVariable Long groupId, @AuthenticationPrincipal CustomUserDetails user);
 
+
+
     @Operation(summary = "방명록 조회하기", description = "마이페이지에 있는 방명록 조회하기.")
+    @Operation(summary = "그룹 방명록 불러오기", description = "group_id에 있는 방명록을 모두 불러옵니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "방명록을 성공적으로 조회했습니다.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseDTO.class),
+                            examples = @ExampleObject(value = """
+                                {
+                                  "status": 200,
+                                  "code": "SUCCESS_RETRIEVE_GUESTBOOK",
+                                  "message": "방명록을 성공적으로 조회했습니다.",
+                                  "data": [
+                                    {
+                                      "volunteer": {
+                                        "userType": "CARE_WORKER",
+                                        "username": "강신영",
+                                        "content": "어우 지겨워"
+                                      },
+                                      "caregiver": {
+                                        "userType": "CAREGIVER",
+                                        "username": "김민주",
+                                        "content": null
+                                      }
+                                    }
+                                  ]
+                                }
+                                """))),
+            @ApiResponse(responseCode = "200", description = "방명록을 성공적으로 조회했으나 리스트가 비어있을 경우",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseDTO.class),
+                            examples = @ExampleObject(value = """
+                            {
+                              "status": 200,
+                              "code": "SUCCESS_BUT_LIST_EMPTY",
+                              "message": "성공적으로 조회하였으나, 리스트가 비었습니다.",
+                              "data": null
+                            }
+                                """))),
+
+            @ApiResponse(responseCode = "401", description = "잘못된 토큰으로 요청할 경우",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDTO.class),
+                            examples = {
+                                    @ExampleObject(name = "INVALID_ACCESS_TOKEN", value = """
+                                        {
+                                            "status": 401,
+                                            "code": "INVALID_ACCESS_TOKEN",
+                                            "message": "유효하지 않은 토큰입니다.",
+                                            "data": null
+                                        }
+                                        """),
+                                    @ExampleObject(name = "TOKEN_EXPIRED", value = """
+                                        {
+                                            "status": 401,
+                                            "code": "TOKEN_EXPIRED",
+                                            "message": "토큰이 만료되었습니다.",
+                                            "data": null
+                                        }
+                                        """),
+                                    @ExampleObject(name = "TOKEN_MISSING", value = """
+                                        {
+                                            "status": 401,
+                                            "code": "TOKEN_MISSING",
+                                            "message": "요청 헤더에 토큰이 없습니다.",
+                                            "data": null
+                                        }
+                                        """)
+                            })),
+
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 access Token으로 요청할 경우",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDTO.class),
+                            examples = {
+                                    @ExampleObject(value = """
+                                        {
+                                            "status": 404,
+                                            "code": "USER_NOT_FOUND",
+                                            "message": "사용자를 찾을 수 없습니다.",
+                                            "data": null
+                                        }
+                                        """)
+                            }))
+    })
     public ResponseEntity<ResponseDTO<?>> getMyPageGuestBook(@AuthenticationPrincipal CustomUserDetails user);
 
 
