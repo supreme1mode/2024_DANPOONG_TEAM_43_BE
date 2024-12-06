596d5dd486c0a9464e9b6bd417dc23472ff44e42
이규민 <90364652+20210815@users.noreply.github.com>
2024-12-07 00:02:55 +0900

    🔒️ [FIX] 웹소켓 추가

--- src/main/java/com/carely/backend/config/SecurityConfig.java
+++ src/main/java/com/carely/backend/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/login", "/error", "/register", "/kakao/login", "/reissue",
                                 "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**"
-                                , "/kakao-code", "/test", "/chat/**", "/certificates/extract", "/test/", "/connect", "/delete-user/**"
+                                , "/kakao-code", "/test", "/chat/**", "/certificates/extract", "/test/", "/connect", "/delete-user/**", "/ws/**"
                         ).permitAll()
                         .anyRequest().authenticated());
 
