b0f45bd1f3bb554a8a891ab7901f66a6a05008aa
이규민 <90364652+20210815@users.noreply.github.com>
2024-12-07 16:31:03 +0900

    🐛 [FIX] UserId를 통해 문서 추출 -> kakaoId로 변경

--- src/main/java/com/carely/backend/controller/DocumentIssuedController.java
+++ src/main/java/com/carely/backend/controller/DocumentIssuedController.java
@@ -23,8 +23,9 @@ import java.util.List;
 public class DocumentIssuedController implements DocumentAPI {
     public final DocumentIssuedService documentIssuedService;
 
-    @GetMapping("/{documentType}")
+    @GetMapping("/{documentType}/userId")
     public ResponseEntity<ResponseDTO<?>> getDocumentIssuedList(@PathVariable String documentType, @AuthenticationPrincipal CustomUserDetails user) {
+        System.out.println(user.getUsername());
         List<ResponseDocumentDTO> list = documentIssuedService.getDocumentIssuedList(user.getUsername(), documentType);
         return ResponseEntity
                 .status(SuccessCode.SUCCESS_RETRIEVE_ACTIVITY_LIST.getStatus().value())
--- src/main/java/com/carely/backend/service/DocumentIssuedService.java
+++ src/main/java/com/carely/backend/service/DocumentIssuedService.java
@@ -30,7 +30,7 @@ public class DocumentIssuedService {
         User user = userRepository.findByKakaoId(kakaoId)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
 
-        List<VolunteerListDTO> list= certificateService.getVolunteerSessionsByUserIdAndType(documentType, user.getId().toString());
+        List<VolunteerListDTO> list= certificateService.getVolunteerSessionsByUserIdAndType(documentType, kakaoId);
         return list.stream().map((volunteerListDTO -> {
             Volunteer volunteer = volunteerRepository.getReferenceById(Long.valueOf(volunteerListDTO.getVolunteerSessionsId()));
             return ResponseDocumentDTO.builder()
